fbe38c87a29555546b9b2bec90b0a238cd07196f
riddhi18dhuri12-coder-op <riddhi18dhuri12@gmail.com>
2026-04-04 23:33:37 +0530

    Update config.js

--- config.js
+++ config.js
@@ -2,3 +2,8 @@ const SUPABASE_CONFIG = {
   url: "https://bhfhtxmgncsyjlgsorap.supabase.co",
   key: "sb_publishable_BCqBJ0MozIk2u5GBy3fAZg_DVzKHyan"
 };
+
+const supabaseClient = supabase.createClient(
+    SUPABASE_CONFIG.url,
+    SUPABASE_CONFIG.key
+);
